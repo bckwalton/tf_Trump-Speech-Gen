4e5009ee4b155f6c07decb91ad37fa406172fcbf
Brian <bckwalton@gmail.com>
2018-07-10 16:14:05 -0400

    Work Continues
    Backing up work, fixed a few bugs. Nothing to write home about. Decreased Neurons per layer just for simplicities sake.

--- Experimental Generator.py
+++ Experimental Generator.py
@@ -28,42 +28,43 @@ Y_modified = np_utils.to_categorical(Y)
 
 #Loading model
 model = Sequential()
-model.add(LSTM(700, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
+model.add(LSTM(50, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(700))
+model.add(LSTM(50))
 model.add(Dropout(0.2))
 model.add(Dense(Y_modified.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-#Train model
-model.fit(X_modified, Y_modified, epochs=1, batch_size=100)
+txt=""
+model.load_weights('./text_generator_50_0.2_50_0.2_baseline.h5')
+for _ in range(1):
+    #Train model
+    model.fit(X_modified, Y_modified, epochs=100, batch_size=100)
 
-#Save model weights
-model.save_weights('./text_generator_700_0.2_700_0.2_baseline.h5')
+    #Save model weights
+    model.save_weights('./text_generator_50_0.2_50_0.2_baseline.h5')
 
+    string_mapped = X[99]
+    full_string = [n_to_char[value] for value in string_mapped]
+    # generating characters
+    print('Generating Characters, Pls wait')
+    for i in range(5000):
+        x = np.reshape(string_mapped,(1,len(string_mapped), 1))
+        x = x / float(len(characters))
+
+        pred_index = np.argmax(model.predict(x, verbose=0))
+        seq = [n_to_char[value] for value in string_mapped]
+        full_string.append(n_to_char[pred_index])
+
+        string_mapped.append(pred_index)
+        string_mapped = string_mapped[1:len(string_mapped)]
+    txt=""
+
+    for char in full_string:
+        txt = txt+char
+    print(txt)
 #Load model weights
-model.load_weights('./text_generator_700_0.2_700_0.2_baseline.h5')
-
-string_mapped = X[99]
-full_string = [n_to_char[value] for value in string_mapped]
-# generating characters
-for i in range(400):
-    x = np.reshape(string_mapped,(1,len(string_mapped), 1))
-    x = x / float(len(characters))
-
-    pred_index = np.argmax(model.predict(x, verbose=0))
-    seq = [n_to_char[value] for value in string_mapped]
-    full_string.append(n_to_char[pred_index])
-
-    string_mapped.append(pred_index)
-    string_mapped = string_mapped[1:len(string_mapped)]
-
+model.load_weights('./text_generator_50_0.2_50_0.2_baseline.h5')
 text_file = open("Output.txt", "w")
 text_file.write(txt)
 text_file.close()
-
-#combining text#combinin
-txt=""
-for char in full_string:
-    txt = txt+char
-print(txt)
